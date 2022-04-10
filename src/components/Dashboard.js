d8a09e01ddc7bd0e101429e8f4925150b40c044a
Devan <devanskeem@gmail.com>
2022-04-10 02:15:20 -0700

    fixed messages format

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,7 +29,7 @@ const Message = ({   time = now,
                      message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam id iste magnam, modi Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam id iste magnam, modi odit quasi sunt unde veritatis voluptas."
 }) => {
     return(
-        <div className="justify-center align-middle flex font-sans p-2">
+        <div className="justify-center align-middle bg-gray-700 rounded m-3 flex font-sans p-2">
             <img src={image} className='w-10 h-10 rounded-full m-2'/>
             <div className="flex-auto">
                 <div className='flex flex-wrap'>
