5e5bff0ee35424e10d041557f4f111957600abc3
Devan <devanskeem@gmail.com>
2022-08-12 20:52:46 -0700

    comments

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import MessageBoard from "./MessageBoard";
 
+//potentially get rid of MessageBoard component and only have Dashboard
 const Dashboard = () => {
     return(
         <div className="flex-auto w-screen ml-16 h-screen text-white">
