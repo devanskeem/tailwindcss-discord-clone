13c59777b6466cab333ba478105b0847c5274bbd
Devan <devanskeem@gmail.com>
2022-08-12 21:00:47 -0700

    changed icons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,11 @@
-import {BsPlus, BsFillLightningFill, BsGearFill} from "react-icons/bs";
-import  {FaFire, FaPoo} from 'react-icons/fa'
+import {BsPlus, BsFillLightningFill, BsGearFill, BsBullseye} from "react-icons/bs";
+import  {FaFire} from 'react-icons/fa'
 
 const SideBar = () => {
   return (
       <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-800 text-white shadow">
           <SideBarIcon icon={<FaFire size="28"  />} text='Fire 🔥' />
-          <SideBarIcon icon={<FaPoo size="28"/>}  text='Poop 💩' />
+          <SideBarIcon icon={<BsBullseye size="28"/>}  text='Bullseye 🎯' />
           <SideBarIcon icon={<BsFillLightningFill size="28" />} text='Lightning ⚡️' />
           <SideBarIcon icon={<BsGearFill size="28" />} text='Settings ⚙️' />
           <SideBarIcon icon={<BsPlus size="48" />} text='Add +' />
